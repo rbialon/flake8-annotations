d5788625809125c17ea16a528c750c92fdf5f746
Raphael Bialon <raphael.bialon@uni-duesseldorf.de>
2020-02-10 10:20:33 +0100

    Use correct directory level for .github directory

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const path = require('path');
-const matchersPath = path.join(__dirname, '..', '.github');
+const matchersPath = path.join(__dirname, '.github');
 
 console.log(`::add-matcher::${path.join(matchersPath, 'flake8-error-problem-matcher.json')}`);
 console.log(`::add-matcher::${path.join(matchersPath, 'flake8-warning-problem-matcher.json')}`);
