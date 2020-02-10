ceff37b2e41a66c7987601e7a62d2bf45c31988c
Raphael Bialon <raphael.bialon@uni-duesseldorf.de>
2020-02-10 12:05:31 +0100

    Change order of problem matcher loading

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 const path = require('path');
 const matchersPath = path.join(__dirname, '.github');
 
-console.log(`::add-matcher::${path.join(matchersPath, 'flake8-error-problem-matcher.json')}`);
 console.log(`::add-matcher::${path.join(matchersPath, 'flake8-warning-problem-matcher.json')}`);
+console.log(`::add-matcher::${path.join(matchersPath, 'flake8-error-problem-matcher.json')}`);
