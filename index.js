6743ab529df1f38b9fb9b37051cdfea0bfaffcf5
Raphael Bialon <raphael.bialon@uni-duesseldorf.de>
2020-02-10 10:00:40 +0100

    Add correct environment to index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-console.log('::add-matcher::.github/flake8-error-problem-matcher.json');
-console.log('::add-matcher::.github/flake8-warning-problem-matcher.json');
+#!/usr/bin/env node
+
+console.log('::add-matcher::${__dirname}/.github/flake8-error-problem-matcher.json');
+console.log('::add-matcher::${__dirname}/.github/flake8-warning-problem-matcher.json');
